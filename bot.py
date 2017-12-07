65dd066050d3d0578f45e0b7d7ff006c8555fabd
Zephyria93 <34337659+Zephyria93@users.noreply.github.com>
2017-12-07 21:24:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,6 @@
-
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+client = Bot(description="Testing", command_prefix="PUT YOUR PREFIX HERE", pm_help = True)
